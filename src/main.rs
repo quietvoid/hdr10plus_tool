4e83006d13db00101aab4ffd3a36591011a79db8
quietvoid <39477805+quietvoid@users.noreply.github.com>
2019-01-14 19:20:21 -0500

    Make struct out of metadata

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{stdout, stdin, Seek, Write, BufRead, Read, BufReader, BufWriter, SeekFrom};
+use std::io::{stdout, stdin, Write, BufRead, BufReader, BufWriter};
 use std::path::Path;
 use serde_json::Value;
 use std::fs::File;
@@ -52,7 +52,7 @@ fn process_input(input: String){
         let metadata_file = format!("{}-meta.json", save_str);
 
         match parse_metadata(path_str, &log_file){
-            Ok(_) => llc_metadata_to_json(&log_file, metadata_file),
+            Ok(_) => llc_read_metadata(&log_file, metadata_file),
             Err(e) => println!("{}", e)
         }
     }
@@ -154,30 +154,33 @@ fn parse_metadata(input: String, log: &String) -> Result<String, std::io::Error>
 }
 
 struct Metadata{
-    BezierCurveData: Vec<u16>,
-    Knee_X: u16,
-    Knee_Y: u16,
-    Average_MaxRGB: u16,
-    MaxScl: Vec<u16>,
-    DistributionIndex: Vec<u8>,
-    DistributionValues: Vec<u32>,
-    TargetedSystemDisplayMaximumLuminance: u32,
-    NumWindows: u8
+    bezier_curve_data: Vec<u16>,
+    knee_x: u16,
+    knee_y: u16,
+    average_maxrgb: u16,
+    maxscl: Vec<u16>,
+    distribution_index: Vec<u8>,
+    distribution_values: Vec<u32>,
+    targeted_system_display_maximum_luminance: u32,
+    num_windows: u8
 }
 
-fn llc_metadata_to_json(input: &String, metadata: String) {
+fn llc_read_metadata(input: &String, save_file: String) {
 
     //Input
     let f = File::open(input).expect("No file found");
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
 
     let maxscl_arr = [1,3,6];
 
     print!("Generating HDR10+ metadata JSON file... ");
     stdout().flush().ok();
 
+    let mut complete_metadata: Vec<Metadata> = Vec::new();
+
     //Loop over lines and read metadata, HDR10+ LLC format
     for line in reader.lines().map(|l| l.unwrap()) {
+
         let line: String = line.chars().skip(1).take(line.len()-2).collect();
 
         let data: Vec<u8> = line.split(", ").map(|x| x.parse::<u8>().unwrap()).collect();
@@ -198,28 +201,23 @@ fn llc_metadata_to_json(input: &String, metadata: String) {
         /*
             For LLC, when 0, skip 1 byte
         */
-        if _targeted_system_display_maximum_luminance == 0{
+        if targeted_system_display_maximum_luminance == 0{
             reader.read_u8(8).unwrap();
         }
 
-        if _targeted_system_display_actual_peak_luminance_flag == 1{
+        if targeted_system_display_actual_peak_luminance_flag == 1{
             println!("Targeted peak flag enabled");
         }
-
-        //println!("NumWindows: {}, Targeted Display Luminance: {}", num_windows, targeted_system_display_maximum_luminance);
 
         let mut average_maxrgb: u16 = 0;
         let mut maxscl: Vec<u16> = Vec::new();
 
-        let mut num_distribution_maxrgb_percentiles= 0;
         let mut distribution_index: Vec<u8> = Vec::new();
         let mut distribution_values: Vec<u32> = Vec::new();
 
-        let mut fraction_bright_pixels = 0;
-
         for _w in 0..num_windows{
             for _i in 0..3{
-                reader.read_u16(1).unwrap(); //maxscl >> 16
+                reader.read_u16(1).unwrap(); //input maxscl >> 16
                 let maxscl_high = reader.read_u16(16).unwrap();
 
                 /*
@@ -231,7 +229,7 @@ fn llc_metadata_to_json(input: &String, metadata: String) {
                     reader.read_u8(1).unwrap();
                     let x = reader.read_u16(7).unwrap();
 
-                    _maxscl.push(x);
+                    maxscl.push(x);
                 }
                 else if maxscl_high == 0{
                     reader.read_u8(8).unwrap();
@@ -242,7 +240,7 @@ fn llc_metadata_to_json(input: &String, metadata: String) {
                 }
             }
 
-            reader.read_u8(1).unwrap();
+            reader.read_u8(1).unwrap(); //input maxrgb >> 16
             average_maxrgb = reader.read_u16(16).unwrap();
 
             /*
@@ -253,33 +251,27 @@ fn llc_metadata_to_json(input: &String, metadata: String) {
                 average_maxrgb = reader.read_u16(8).unwrap();
             }
 
-            num_distribution_maxrgb_percentiles = reader.read_u8(4).unwrap();
+            let num_distribution_maxrgb_percentiles = reader.read_u8(4).unwrap();
 
             for _i in 0..num_distribution_maxrgb_percentiles{
                 distribution_index.push(reader.read_u8(7).unwrap());
                 distribution_values.push(reader.read_u32(17).unwrap());
             }
 
-            fraction_bright_pixels = reader.read_u16(10).unwrap();
+            reader.read_u16(10).unwrap(); //fraction_bright_pixels, unused for now
         }
-
-        //println!("AverageRGB: {}, MaxScl: {:?}", _average_maxrgb, _maxscl);
-        //println!("NumPercentiles: {}\nDistributionIndex: {:?}\nDistributionValues: {:?}", num_distribution_maxrgb_percentiles, distribution_index, distribution_values);
 
         let mastering_display_actual_peak_luminance_flag = reader.read_u8(1).unwrap();
 
         //0 for now
-        if _mastering_display_actual_peak_luminance_flag == 1{
+        if mastering_display_actual_peak_luminance_flag == 1{
             println!("Mastering peak flag enabled");
         }
 
         let mut knee_point_x: u16 = 0;
         let mut knee_point_y: u16 = 0;
-        let mut num_bezier_curve_anchors: u8 = 0;
 
         let mut bezier_curve_anchors: Vec<u16> = Vec::new();
-
-        let mut color_saturation_mapping_flag: u8 = 0;
 
         for _w in 0..num_windows{
             let tone_mapping_flag = reader.read_u8(1).unwrap();
@@ -288,108 +280,129 @@ fn llc_metadata_to_json(input: &String, metadata: String) {
 
                 knee_point_x = reader.read_u16(12).unwrap();
                 knee_point_y = reader.read_u16(12).unwrap();
-                num_bezier_curve_anchors = reader.read_u8(4).unwrap();
+                let num_bezier_curve_anchors = reader.read_u8(4).unwrap();
 
                 for _i in 0..num_bezier_curve_anchors{
                     bezier_curve_anchors.push(reader.read_u16(10).unwrap());
                 }
-
-                //println!("Knee_X: {}, Knee_Y: {}, Anchors: {:?}\n", knee_point_x, knee_point_y, bezier_curve_anchors);
             }
         }
 
-        color_saturation_mapping_flag = reader.read_u8(1).unwrap();
+        let color_saturation_mapping_flag = reader.read_u8(1).unwrap();
 
         //0 for now
         if color_saturation_mapping_flag == 1{
             println!("Color saturation mapping flag enabled");
         }
+
+        let meta = Metadata {
+            num_windows: num_windows,
+            targeted_system_display_maximum_luminance: targeted_system_display_maximum_luminance,
+            average_maxrgb: average_maxrgb,
+            maxscl: maxscl,
+            distribution_index: distribution_index,
+            distribution_values: distribution_values,
+            knee_x: knee_point_x,
+            knee_y: knee_point_y,
+            bezier_curve_data: bezier_curve_anchors
+        };
+
+        complete_metadata.push(meta);
     }
+
+    write_json(save_file, complete_metadata);
 
     println!("Done.");
 }
 
-fn write_json(input: String, meta: Vec<Metadata>){
-    let save_file = File::create(metadata).expect("Can't create file");
+fn write_json(input: String, metadata: Vec<Metadata>) {
+    let save_file = File::create(input).expect("Can't create file");
     let mut writer = BufWriter::with_capacity(10000000, save_file);
 
     if let Err(_) = writeln!(writer, "{{\n\t\"SceneInfo\": ["){
         eprintln!("Couldn't write to file");
     }
 
-    //Prepare BezierCurveData JSON string
-    let mut anchors_str = String::new();
-    for a in 0..num_bezier_curve_anchors{
-        let anchor_v = bezier_curve_anchors[a as usize];
+    let mut frame = 0;
+    let max_frames = metadata.len() - 1;
 
-        let mut anchor = format!("\t{}, \n", anchor_v);
+    for m in metadata{
 
-        if a == num_bezier_curve_anchors - 1 {
-            anchor = format!("\t{}", anchor_v);
+        //Prepare bezier_curve_data JSON string
+        let mut anchors_str = String::new();
+        let num_bezier_curve_anchors = m.bezier_curve_data.len();
+        for a in 0..num_bezier_curve_anchors{
+            let anchor_v = m.bezier_curve_data[a as usize];
+
+            let mut anchor = format!("\t{}, \n", anchor_v);
+
+            if a == num_bezier_curve_anchors - 1 {
+                anchor = format!("\t{}", anchor_v);
+            }
+
+            anchors_str.push_str(anchor.as_str());
         }
 
-        anchors_str.push_str(anchor.as_str());
-    }
+        //Prepare Distribution JSON string
+        let mut index_str = String::new();
+        let mut values_str = String::new();
+        let num_distribution_maxrgb_percentiles = m.distribution_values.len();
 
-    //Prepare Distribution JSON string
-    let mut index_str = String::new();
-    let mut values_str = String::new();
-    for a in 0..num_distribution_maxrgb_percentiles{
-        let index_v = _distribution_index[a as usize];
-        let values_v = _distribution_values[a as usize];
+        for a in 0..num_distribution_maxrgb_percentiles{
+            let index_v = m.distribution_index[a as usize];
+            let values_v = m.distribution_values[a as usize];
 
-        let mut index = format!("\t{},\n", index_v);
-        let mut values = format!("\t{},\n", values_v);
+            let mut index = format!("\t{},\n", index_v);
+            let mut values = format!("\t{},\n", values_v);
 
-        if a == num_distribution_maxrgb_percentiles - 1 {
-            index = format!("\t{}", index_v);
-            values = format!("\t{}", values_v);
+            if a == num_distribution_maxrgb_percentiles - 1 {
+                index = format!("\t{}", index_v);
+                values = format!("\t{}", values_v);
+            }
+
+            index_str.push_str(index.as_str());
+            values_str.push_str(values.as_str());
         }
 
-        index_str.push_str(index.as_str());
-        values_str.push_str(values.as_str());
-    }
+        //Prepare MaxScl JSON string
+        let mut maxscl_str: String = String::new();
+        for a in 0..3{
+            let value = m.maxscl[a as usize];
+            let mut maxscl_l = format!("\t{},\n", value);
 
-    //Prepare MaxScl JSON string
-    let mut maxscl_str: String = String::new();
-    for a in 0..3{
-        let value = _maxscl[a as usize];
-        let mut maxscl_l = format!("\t{},\n", value);
-
-        if a == 2{
-            maxscl_l = format!("\t{}", value);
+            if a == 2{
+                maxscl_l = format!("\t{}", value);
+            }
+            maxscl_str.push_str(maxscl_l.as_str());
         }
-        maxscl_str.push_str(maxscl_l.as_str());
-    }
 
-    let bezier_data = format!("\"BezierCurveData\": {{\n\"Anchors\": [\n{}\n],\n\"KneePointX\": {},\n\"KneePointY\": {}\n}},\n", anchors_str, _knee_point_x, _knee_point_y);
-    let luminance_data = format!("\"LuminanceParameters\": {{\n\"AverageRGB\": {},\n\"LuminanceDistributions\": {{\n\"DistributionIndex\": [\n{}\n],\n\"DistributionValues\": [\n{}\n]\n}},\n\"MaxScl\": [\n{}\n]\n}},\n", _average_maxrgb, index_str, values_str, maxscl_str);
-    let windows_data: String = format!("\"NumberOfWindows\": {},\n\"TargetedSystemDisplayMaximumLuminance\": {}", _num_windows, _targeted_system_display_maximum_luminance);
+        let bezier_data = format!("\"BezierCurveData\": {{\n\"Anchors\": [\n{}\n],\n\"KneePointX\": {},\n\"KneePointY\": {}\n}},\n", anchors_str, m.knee_x, m.knee_y);
+        let luminance_data = format!("\"LuminanceParameters\": {{\n\"AverageRGB\": {},\n\"LuminanceDistributions\": {{\n\"DistributionIndex\": [\n{}\n],\n\"DistributionValues\": [\n{}\n]\n}},\n\"MaxScl\": [\n{}\n]\n}},\n", m.average_maxrgb, index_str, values_str, maxscl_str);
+        let windows_data: String = format!("\"NumberOfWindows\": {},\n\"TargetedSystemDisplayMaximumLuminance\": {}", m.num_windows, m.targeted_system_display_maximum_luminance);
 
-    let final_str;
+        let final_str;
 
-    //Only add BezierCurveData JSON if it's available, no empty array.
-    if num_bezier_curve_anchors != 0{
-        final_str = format!("{{\n{}{}{}\n}}", bezier_data, luminance_data, windows_data);
-    }
-    else{
-        final_str = format!("{{\n{}{}\n}}", luminance_data, windows_data);
-    }
+        //Only add bezier_curve_data JSON if it's available, no empty array.
+        if num_bezier_curve_anchors != 0{
+            final_str = format!("{{\n{}{}{}\n}}", bezier_data, luminance_data, windows_data);
+        }
+        else{
+            final_str = format!("{{\n{}{}\n}}", luminance_data, windows_data);
+        }
 
-    //println!("{}", final_str);
+        let json: Value = serde_json::from_str(&final_str).unwrap();
 
-    let json: Value = serde_json::from_str(&final_str).unwrap();
+        let mut json_final = serde_json::to_string_pretty(&json).unwrap();
 
-    let mut json_final = serde_json::to_string_pretty(&json).unwrap();
+        if frame < max_frames{
+            json_final.push(',');
+        }
 
-    if frame != number_frames{
-        json_final.push(',');
-    }
+        frame += 1;
 
-    frame += 1;
-
-    if let Err(_) = writeln!(writer, "{}", json_final){
-        eprintln!("Couldn't write to file");
+        if let Err(_) = writeln!(writer, "{}", json_final){
+            eprintln!("Couldn't write to file");
+        }
     }
 
     if let Err(_) = writeln!(writer, "]\n}}"){
